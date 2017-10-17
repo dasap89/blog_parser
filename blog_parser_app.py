d688c1ebb33a28491c2c4e7747037f3ee087f6f1
dasap89 <dmytro.sapotnitskiy@gmail.com>
2017-10-18 01:54:30 +0300

    changes added to main parser to increase speed

--- blog_parser_app.py
+++ blog_parser_app.py
@@ -1,12 +1,13 @@
 """Application for parsing blog articles https://realpython.com/blog/"""
 
 from flask import Flask, render_template,\
-    request, flash, url_for, redirect, abort
+    request, flash, url_for, redirect, abort, jsonify
 from flask_mongoengine import MongoEngine
 import lxml.html
 import requests
 from bson.objectid import ObjectId
 from config import config
+from bson import ObjectId
 
 # init flask app
 app = Flask(__name__)
@@ -18,10 +19,14 @@ db.init_app(app)
 
 main_url = "https://realpython.com"
 blog_url = 'https://realpython.com/blog'
-path_all_categories = ".//ul[@id='category']//li//a"
-path_all_articles =\
-    ".//div[@id='blog-archives']//article[@class='page-header']//h1//a"
-path_article_text = ".//div[@class='entry-content clearfix']"
+path_to_all_articles = '//article[@class="page-header"]'
+path_to_categories_for_article = './/span[@class="categories"]//a'
+
+
+class Categories(db.Document):
+    """Make mongodb model for categories"""
+    title = db.StringField()
+    url_category = db.URLField()
 
 
 class Articles(db.Document):
@@ -29,13 +34,8 @@ class Articles(db.Document):
     category = db.StringField()
     title = db.StringField()
     text = db.StringField()
+    categories =  db.ListField(db.ReferenceField(Categories))
     url_article = db.URLField()
-
-
-class Categories(db.Document):
-    """Make mongodb model for categories"""
-    title = db.StringField()
-    url_category = db.URLField()
 
 
 def get_content(url):
@@ -67,86 +67,51 @@ def parse(html, path):
     return result
 
 
-def parse_categories_list(url):
+def parse_all_articles(url):
     """
-    Add to mongo db categories if it doesnt exist
+    Add to mongo db articles and categories per article if it doesnt exist
     :param url - url current category:
     :return list categories with urls:
     """
     content = get_content(url)
-    result = parse(content, path_all_categories)
-    categories = []
+    result = parse(content, path_to_all_articles)
     if result:
-        try:
+        try:     
             for item in result:
-                if not Categories.objects(title=item.text_content()):
-                    # add category to db
-                    Categories(title=item.text_content(),
-                               url_category='{0}{1}'.format(
-                                   main_url, item.get('href'))).save()
-                category = dict()
-                category['category'] = item.text_content()
-                category['url'] = '{0}{1}'.format(main_url,
-                                                  item.get('href'))
-                categories.append(category)
+
+                item = item.getchildren()
+                article, categories = item
+                categories = categories.xpath(path_to_categories_for_article)
+
+                article_title = article.text_content()
+
+
+                if not Articles.objects(title=article_title):
+
+                    Articles(
+                        title=article_title,
+                        url_article='{0}{1}'.format(main_url, article.getchildren()[0].get('href')),
+                    ).save()
+                
+                article_obj = Articles.objects(title=article_title)
+
+                
+                for category in categories:
+                    category_title = category.text_content()                
+
+                    if not Categories.objects(title=category_title):
+                        # add category to db
+                        Categories(title=category_title, url_category='{0}{1}'.format(main_url, category.get('href'))).save()
+  
+                    category_obj = Categories.objects.get(title=category_title)
+
+                    if category_obj not in Articles.objects.get(title=article_title).categories:
+                        article_obj.update_one(push__categories=[category_obj])
+            
         except Exception:
             abort(500)
-    return categories
-
-
-def parse_articles_list(cat_list):
-    """
-    Add to mongo db articles if it doesnt exist
-    :param cat_list - list categories with urls:
-    :return:
-    """
-    if cat_list:
-        try:
-            for category in cat_list:
-                content = get_content(category['url'])
-                result = parse(content, path_all_articles)
-                for item in result:
-                    if not Articles.objects(title=item.text_content()):
-                        url_article = '{0}{1}'.format(main_url,
-                                                      item.get('href'))
-                        # add article to db
-                        Articles(
-                            category=category['category'],
-                            title=item.text_content(),
-                            url_article=url_article,
-                            text=parse_article_text(url_article)
-                        ).save()
-
-        except Exception:
-            abort(500)
-
-
-def parse_article_text(url):
-    """
-    returning article text
-    :param url - article url:
-    :return string with article text:
-    """
-    if url:
-        try:
-            content = get_content(url)
-            result = parse(content, path_article_text)
-            content = ''
-            for item in result:
-                content += item.text_content()
-        except Exception:
-            abort(500)
-    return content
-
-
-def parse_all(url):
-    """
-    full cycle parsing(categories adn articles)
-    :param url:
-    :return:
-    """
-    categories_list = parse_categories_list(url)
-    parse_articles_list(categories_list)
+    else:
+        return abort(500)    
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -155,14 +120,17 @@ def index():
     start page
     :return: page with total count articles and categoriesin db
     """
+    
     try:
         total_categories = Categories.objects.count()
         total_articles = Articles.objects.count()
         if request.method == 'POST':
             # add task to celery
             flash('All data you will see after few minutes, refresh page.')
-            parse_all(blog_url)
-            
+
+            # parse_all(blog_url)
+            parse_all_articles(blog_url)
+
             return redirect(url_for(
                 'index',
                 categories=total_categories,
@@ -171,7 +139,7 @@ def index():
 
     except Exception:
         abort(404)
-
+    
     return render_template(
         'index.html',
         categories=total_categories,
@@ -185,14 +153,19 @@ def categories():
     categories page
     :return: list all categories from db
     """
-    try:
-        categories = Categories.objects()
-    except Exception:
-        abort(404)
-    return render_template(
-        'categories.html',
-        categories=categories
-    )
+    # category = request.args.get("category", None)
+    # print category
+    # if category:
+    #     try:
+    #         category = Categories.objects.get(title=category)
+    #         print category
+    #         articles = Articles.objects.filter(categories__in=[category])
+    #     except Exception:
+    #         abort(404)
+    #     return render_template('categories.html', categories=category, articles=articles)
+    # else:
+    categories = Categories.objects()
+    return render_template('categories.html', categories=categories)
 
 
 @app.route('/articles/', methods=['GET'])
@@ -206,7 +179,8 @@ def articles():
     article_id = request.args.get("article", None)
     try:
         if category:
-            articles = Articles.objects(category=category)
+            category = Categories.objects.get(title=category)
+            articles = Articles.objects.filter(categories__in=[category])
         elif article_id and ObjectId.is_valid(article_id):
             article = Articles.objects.get(id=ObjectId(article_id))
             return render_template('article.html', article=article)
@@ -221,6 +195,53 @@ def articles():
     )
 
 
+# APIs
+
+@app.route('/api/categories/', methods=['GET'])
+def api_get_categories():
+    """
+    articles page
+    :return all articles from db, or current articles with current category,
+    or current article(dependency from request params)
+    """
+    categories = Categories.objects()
+    return jsonify({'categories': categories})
+
+
+@app.route('/api/categories/<string:category_name>', methods=['GET'])
+def api_get_articles_by_category(category_name):
+    """
+    articles page
+    :return all articles from db, or current articles with current category,
+    or current article(dependency from request params)
+    """
+    category = Categories.objects.get(title=category_name)
+    articles = Articles.objects.filter(categories__in=[category])
+    return jsonify({'category': category, 'articles': articles})
+
+
+@app.route('/api/articles/', methods=['GET'])
+def api_get_articles():
+    """
+    articles page
+    :return all articles from db, or current articles with current category,
+    or current article(dependency from request params)
+    """
+    articles = Articles.objects()
+    return jsonify({'articles': articles})
+
+@app.route('/api/articles/<string:article_id>', methods=['GET'])
+def api_get_one_article(article_id):
+    try:
+        article_id = ObjectId(article_id)
+        article = Articles.objects.get(id=article_id)
+        return jsonify({'article': article})
+    except:
+        return jsonify({'article': "Not found article with id {}".format(article_id)})
+
+
+# Error handlers
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('error_404.html'), 404
@@ -232,4 +253,4 @@ def server_err(error):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True)
